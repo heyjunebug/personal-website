fc8253ec529ea0597698714a2d587ceacaea7ba9
heyjunebug <79528923+heyjunebug@users.noreply.github.com>
2023-10-06 10:42:20 -0700

    Added robots.txt and updated metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
 	title: 'June Like the Month',
-	url: "https://heyjunebug.com/",
+	url: "https://heyjunebug.com",
 	language: "en",
 	description: "The personal blog and portfolio of June Umfress, a designer and writer who lives in the San Francisco Bay Area.",
 	author: {
